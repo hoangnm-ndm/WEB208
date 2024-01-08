9dc6e8040052ecd862e86851b2293d4240ff62c4
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-01-08 07:54:48 +0700

    create base

---
+++ note.md
@@ -0,0 +1,19 @@
+### 1: Gỡ bản cũ:
+
+```
+npm uninstall -g @angular/cli
+```
+
+### 2: Cài bản mới:
+
+```
+npm install -g @angular/cli@16
+```
+
+```
+ng v
+```
+
+```
+ng new ten_du_an
+```
---
+++ src/interfaces/Product.ts
@@ -0,0 +1,11 @@
+interface Product {
+  id?: string;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+  category: string;
+  quantity: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
