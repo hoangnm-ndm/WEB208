d8a57518b943775bb9e4904acb0f40e7dcced98e
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-02-18 19:08:15 +0700

    done-demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -30,6 +30,7 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes, { enableTracing: true }),
   ],
   providers: [],
--- src/app/pages/product-form/product-form.component.ts
+++ src/app/pages/product-form/product-form.component.ts
@@ -1,5 +1,6 @@
 // product-form.component.ts
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from 'src/app/interfaces/product';
 import { ProductService } from 'src/app/services/products.service';
@@ -16,13 +17,22 @@ export class ProductFormComponent implements OnInit {
     description: '',
     thumbnail: '',
   };
-  isEditMode: boolean = false;
 
+  isEditMode: boolean = false;
+  productForm: FormGroup;
   constructor(
     private productService: ProductService,
     private route: ActivatedRoute,
-    private router: Router
-  ) {}
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.productForm = this.fb.group({
+      title: [this.product.title, Validators.required],
+      price: [this.product.price, [Validators.required, Validators.min(0)]],
+      description: [this.product.description],
+      thumbnail: [this.product.thumbnail],
+    });
+  }
 
   ngOnInit() {
     const productId = this.route.snapshot.params['id'];
@@ -30,19 +40,37 @@ export class ProductFormComponent implements OnInit {
       this.isEditMode = true;
       this.productService.getProduct(productId).subscribe((product) => {
         this.product = product;
+        this.productForm.setValue({
+          title: this.product.title,
+          price: this.product.price,
+          description: this.product.description,
+          thumbnail: this.product.thumbnail,
+        });
       });
     }
   }
 
   saveProduct() {
-    if (this.isEditMode) {
-      this.productService.updateProduct(this.product).subscribe(() => {
-        this.router.navigate(['/admin']);
-      });
-    } else {
-      this.productService.addProduct(this.product).subscribe(() => {
-        this.router.navigate(['/admin']);
-      });
+    // Kiểm tra xem FormGroup có hợp lệ không
+    if (this.productForm.valid) {
+      // Lấy giá trị từ FormGroup
+      const formValue = this.productForm.value;
+
+      // Gán giá trị từ FormGroup vào đối tượng product
+      this.product.title = formValue.title;
+      this.product.price = formValue.price;
+      this.product.description = formValue.description;
+      this.product.thumbnail = formValue.thumbnail;
+
+      if (this.isEditMode) {
+        this.productService.updateProduct(this.product).subscribe(() => {
+          this.router.navigate(['/']);
+        });
+      } else {
+        this.productService.addProduct(this.product).subscribe(() => {
+          this.router.navigate(['/']);
+        });
+      }
     }
   }
 }
