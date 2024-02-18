befa69d8a57d7e2aef5351d7141271d08f97b5e0
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-02-18 18:26:14 +0700

    remove redundant component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,29 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { AboutPageComponent } from './pages/about-page/about-page.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { AdminProductListComponent } from './pages/admin/admin-product-list/admin-product-list.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { ShopPageComponent } from './pages/shop-page/shop-page.component';
-import { HeaderComponent } from './components/header/header.component';
-import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ProductFormComponent } from './pages/admin/product-form/product-form.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { ProductDetailComponent } from './pages/admin/product-detail/product-detail.component';
+import { AppComponent } from './app.component';
+import { AdminProductListComponent } from './pages/admin-product-list/admin-product-list.component';
+import { ProductFormComponent } from './pages/product-form/product-form.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 
 const appRoutes: Routes = [
-  { path: '', component: HomePageComponent },
-  { path: 'shop', component: ShopPageComponent },
-  { path: 'about', component: AboutPageComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: 'home', redirectTo: '/', pathMatch: 'full' },
-  { path: 'admin', component: AdminProductListComponent },
-  { path: 'admin/products/add', component: ProductFormComponent },
-  { path: 'admin/products/edit/:id', component: ProductFormComponent },
+  { path: '', component: AdminProductListComponent },
+  { path: 'add', component: ProductFormComponent },
+  { path: 'edit/:id', component: ProductFormComponent },
+  { path: 'detail/:id', component: ProductDetailComponent },
   { path: '**', component: NotFoundPageComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutPageComponent,
-    HeaderComponent,
-    LoginComponent,
-    RegisterComponent,
     AdminProductListComponent,
-    ContactComponent,
-    ShopPageComponent,
-    NotFoundPageComponent,
     ProductFormComponent,
-    ProductListComponent,
-    HomePageComponent,
+    NotFoundPageComponent,
     ProductDetailComponent,
   ],
   imports: [
