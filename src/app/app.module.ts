a81d993e5e47323d29ba21ded54c0ddfcb0aa013
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-02-19 08:39:40 +0700

    create base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,27 +5,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { AdminProductListComponent } from './pages/admin-product-list/admin-product-list.component';
-import { ProductFormComponent } from './pages/product-form/product-form.component';
-import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
-import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
-
-const appRoutes: Routes = [
-  { path: '', component: AdminProductListComponent },
-  { path: 'add', component: ProductFormComponent },
-  { path: 'edit/:id', component: ProductFormComponent },
-  { path: 'detail/:id', component: ProductDetailComponent },
-  { path: '**', component: NotFoundPageComponent },
-];
+const appRoutes: Routes = [];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AdminProductListComponent,
-    ProductFormComponent,
-    NotFoundPageComponent,
-    ProductDetailComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     FormsModule,
